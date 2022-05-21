b739ad18e30e1941875f9afab6ed745ef829af56
Jugger95 <103952561+Jugger95@users.noreply.github.com>
2022-05-21 14:14:34 -0800

    fixed a bug

--- GameDev.tv Game Jam Game/Assets/EndOfGame.cs
+++ GameDev.tv Game Jam Game/Assets/EndOfGame.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndOfGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int EndOfGameBuildNumber;
+    void OnCollisionEnter2D(Collision2D coll)
     {
-        
+        if(coll.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(EndOfGameBuildNumber);
+        }
     }
 }
